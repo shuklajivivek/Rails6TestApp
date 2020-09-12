c196959636158860ba1c07c1a97dad97c4dbe135
vivekshukla <vivekshukla.nit@gmail.com>
2020-09-12 15:53:43 +0530

    added has many Association to user model and article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_142124) do
+ActiveRecord::Schema.define(version: 2020_09_12_101609) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "decription"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
